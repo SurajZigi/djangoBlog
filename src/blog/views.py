6828f70b6890de2ecd1272ec68c8e258c485a502
surajknite@gmail.com <surajknite@gmail.com>
2020-01-02 23:36:51 +0530

    layout.html is added and templte inheritace is working

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Suraj',
+        'title': 'Blog Post 1',
+        'content': 'First post Content',
+        'date_posted': 'January 1, 2020'
+    },
+    {
+        'author': 'Aman',
+        'title': 'Blog Post 2',
+        'content': 'Second post Content',
+        'date_posted': 'January 2, 2020'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
      return render(request, 'blog/about.html')

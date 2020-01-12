3bbe7bdbb69c7bc3e1d221c893eed5bbacd266c4
surajknite@gmail.com <surajknite@gmail.com>
2020-01-12 22:44:51 +0530

    loin and logout message is working import django.contrib message to views.py

--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
+from django.contrib import messages
 
  
 # Create your views here. 
@@ -8,8 +9,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
-            messages.success(request, f'Account created for { username }!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created now you can login')
+            return redirect('login')
     else: 
         form = UserRegisterForm()
     return render(request,'users/register.html', {'form': form})

0c1f6ae409200ce711184524fa8a4987b081cd9f
surajknite@gmail.com <surajknite@gmail.com>
2020-01-13 13:00:43 +0530

    profile update section is addes and its working and Updataing

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
  
@@ -10,6 +9,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f'Your account has been created now you can login')
             return redirect('login')
@@ -19,4 +19,11 @@ def register(request):
  
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request,'users/profile.html', context)

9626f1376f54d281584218340c954c585f330050
surajknite@gmail.com <surajknite@gmail.com>
2020-01-15 02:37:14 +0530

    user PostView is added user_post.html

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView, 
     DetailView, 
@@ -33,7 +34,7 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-    model  = Post
+    model  = Post 
     fields = ['title', 'content']
 
     def form_valid(self,form):
@@ -64,6 +65,16 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
         if self.request.user == post.author:
             return True 
         return False
+
+class UserPostListView(ListView):
+    model  = Post
+    template_name = 'blog/user_post.html'
+    context_object_name ='posts' 
+    paginate_by = 5
+
+    def get_query_set(self): 
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 def about(request):
      return render(request, 'blog/about.html',{'title':'About'})

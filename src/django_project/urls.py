ed31ebf831cc19443405aec22eb5e8636b62b41c
surajknite@gmail.com <surajknite@gmail.com>
2020-01-16 23:13:27 +0530

    Password rest email send routes are created

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('profile/', user_views.profile, name='profile'),
-
-]
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+ ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    

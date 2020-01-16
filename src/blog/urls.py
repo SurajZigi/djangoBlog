efb5f1cbf7cdbfab464e00cf21cf8c969419f50c
surajknite@gmail.com <surajknite@gmail.com>
2020-01-16 20:51:39 +0530

    update post url path vew is changed and operationable

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,7 +4,7 @@ from .views import (
     PostDetailView, 
     PostCreateView, 
     PostUpdateView,
-    PostDeleteView,
+    PostDeleteView, 
     UserPostListView
 )
 # Create your views here.
@@ -14,8 +14,8 @@ urlpatterns = [
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostCreateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
- ]
+ ] 
 #<app>/<model>_<viewtype>.html 
